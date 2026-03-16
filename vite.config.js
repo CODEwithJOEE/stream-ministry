403abeaee8c1b623a66f4f884b9f4728e0b241d5
Joemarie <joemarie.r@techguys.work>
2026-03-16 14:36:28 +0800

    add pwa plugin, and icons for the app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Stream Ministry",
+        short_name: "Stream",
+        description: "Recovery Version Bible Study and Hymns",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "Stream-Ministry-icon.png", // Using your existing file
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "Stream-Ministry-icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
